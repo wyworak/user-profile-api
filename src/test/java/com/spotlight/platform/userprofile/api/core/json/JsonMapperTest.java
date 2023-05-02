e7f2fcdb6d3d15d03f3a20b79c40e364ad1c02eb
Willyan Schultz Dworak <wyworak@gmail.com>
2023-05-02 21:25:46 +0200

    Initial commit

---
+++ src/test/java/com/spotlight/platform/userprofile/api/core/json/JsonMapperTest.java
@@ -0,0 +1,26 @@
+package com.spotlight.platform.userprofile.api.core.json;
+
+import com.spotlight.platform.helpers.FixtureHelpers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JsonMapperTest {
+
+    private static final String FIXTURE_PATH = "/fixtures/core/json/testEntityWithInstantsIsoString.json";
+
+    @Test
+    void serializeInstants_CorrectlySerialized() throws Exception {
+        var serializedEntity = JsonMapper.getInstance().writeValueAsString(new TestEntityWithInstants());
+
+        assertThat(serializedEntity).isEqualToIgnoringWhitespace(FixtureHelpers.fixture(FIXTURE_PATH));
+    }
+
+    @Test
+    void deserializeInstants_CorrectlyDeserialized() throws Exception {
+        var deserializedEntity = JsonMapper.getInstance().readValue(FixtureHelpers.fixture(FIXTURE_PATH), TestEntityWithInstants.class);
+
+        assertThat(deserializedEntity).usingRecursiveComparison().isEqualTo(new TestEntityWithInstants());
+    }
+}
---
+++ src/test/java/com/spotlight/platform/userprofile/api/web/healthchecks/PreventStartupWarningHealthCheckTest.java
@@ -0,0 +1,22 @@
+package com.spotlight.platform.userprofile.api.web.healthchecks;
+
+import com.codahale.metrics.health.HealthCheck;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PreventStartupWarningHealthCheckTest {
+
+    private final PreventStartupWarningHealthCheck healthCheck = new PreventStartupWarningHealthCheck();
+
+    @Test
+    void healthcheckName_isCorrect() {
+        assertThat(PreventStartupWarningHealthCheck.NAME).isEqualTo("preventing-startup-warning-healthcheck");
+    }
+
+    @Test
+    void healthCheckCalled_returnsHealthy() {
+        assertThat(healthCheck.check()).isEqualTo(HealthCheck.Result.healthy());
+    }
+}
